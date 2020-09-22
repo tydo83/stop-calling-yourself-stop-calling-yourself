6c63d59d68413ba9ec894c13338901822bf661bd
Timothy Do <tydo83@gmail.com>
2020-09-22 14:48:24 -0400

    I'm gonna commit it anyway

---
+++ main.js
@@ -0,0 +1,52 @@
+function add3QuestionMark(str){
+    return str + '???'    
+}
+let result1 = add3QuestionMark('hello');
+let result2 = add3QuestionMark('what even');
+result1;
+result2;
+
+function add2Strings(str1, str2){
+    return str1 + ' ' + str2;
+}
+let result3 = add2Strings('oh', 'hi');
+let result4 = add2Strings('well', 'hello back');
+result3;
+result4;
+
+function sub3Numbers(num1, num2, num3){
+    return num1 - num2 - num3;
+}
+let result5 = sub3Numbers(10, 1, 2); 
+let result6 = sub3Numbers(8, 3, 5);
+let result7 = sub3Numbers(100, 80, 25);
+result5;
+result6;
+result7;
+
+function sub3NumbersR(num1, num2, num3){
+    return num3 - num2 - num1;
+}
+let result8 = sub3NumbersR(10, 1, 2);
+let result9 = sub3NumbersR(2, 1, 10);
+result8;
+result9;
+
+function tripleString(str){
+    return str + str + str;
+}
+let result10 = tripleString('codeimmersives');
+let result11 = tripleString('is the best');
+result10;
+result11;
+
+function squareRoot(num){
+    return Math.sqrt(num);
+}
+let result12 = squareRoot(16); 
+let result13 = squareRoot(4);
+let result14 = squareRoot(9);
+result12;
+result13;
+result14;
+
